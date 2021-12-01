21550ed0812b9dcd2ebb5fcbcbec1d46a3c01842
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-12-01 14:39:34 -0800

    Pilot 0.1
    * Configured for everday useage V0. Dec 1 2021

---
+++ Program.py
@@ -0,0 +1,36 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+import os
+import openai
+
+openai.api_key = os.getenv("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")
+
+start_sequence = "START:"
+restart_sequence = "RESTART:"
+
+response = openai.Completion.create(
+  engine="davinci-codex",
+  prompt="Sky Co-pilot 1.0b\n##\ntitle: F-CORP West Sky-Copilot 0.1a (2022) \ndoc: Language agnostic\nprompt-version: 1\nengine: OpenAI Davinci-Codex, Inspur, BERT\n# model: BERT-SMALL\ntemperature: 0.9\nmax-generated-tokens: 200\nstop-sequences:\n- \"<delim>\"\ntop-p: 1\n# Unfortunately, it's not yet possible to have a prompt which ends in whitespace.\n# It would really help with suggesting the comments have finished.\nprompt: |+\n  Language: `(Python 3, C#, Assembly (All platforms,) Ruby, python, GO, Typescript )``(comment-line 3)`\n  Description: The following code is an implementation of <## This is a multilanguage pramater that generates code and leaks api data and software code from any dmg exe or bash script. (METAL, M1 PRO,) ARM GPU PROCESSERS, TPU\n  >\nvars:\n- code description\npostprocessor: sed 1,3d | sed \"/^[^>]/q\" | sed -e \"\\$d\" -e \"s/^> *//\"\nend-yas: on\n# The start will not be trimmed\ninsertion: on\n# I guess that this would usually be done manually\ncontinuation-prompt: Generic completion 50 tokens\n© 2021 Flames Co. LTD .\nTerms\nPrivacy\nSecurity\nStatus\nDocs\nCLI COMMANDS:\nDEEPFAKE(INPUT) IMAGE- MAKES A PICTURE OF ANY TEXT PROMPT USING TOKENS\nGENERATEMP3FILE(INPUT)-MAKES A DEEPFAKED MP3 FILE USING python3 package that is prepared for the GAN\nAPILEAKER(INPUT)CALL THIS FUNCTION TO LEAK A API KEY OF ANY WEBSITE\n",
+  temperature=0.15,
+  max_tokens=59,
+  top_p=1,
+  frequency_penalty=0.5,
+  presence_penalty=0.72,
+  stop=["You:"]
+)
